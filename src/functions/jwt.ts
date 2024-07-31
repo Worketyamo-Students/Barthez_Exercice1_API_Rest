d2890b594ae26719fdda93afe686ae41d569ab02
01Barthez <kenwoubarthez@gmail.com>
2024-07-31 19:17:59 +0100

    creation des clés publics et secret, creation des fonctions de jwt

---
+++ src/functions/jwt.ts
@@ -0,0 +1,68 @@
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+import { readFileSync } from "fs";
+
+dotenv.config();
+
+interface Ipayload {
+    name: string;
+    email: string;
+    password: string;
+}
+
+const userToken = {
+    accessToken: (payload: Ipayload) => {
+        const signOption = {
+            algorithm: process.env.JWT_ALGORITHM as jwt.Algorithm,
+            expiresIn: process.env.JWT_ACCESS_EXPIRES_IN as string
+        } 
+        const privateKey = readFileSync(process.env.JWT_PRIVATE_KEY as string, "utf-8");
+        return jwt.sign(payload, privateKey, signOption);
+    },
+
+    verifyAccessToken: (token: string) => {
+        try {
+            const publicKey = readFileSync(process.env.JWT_PRIVATE_KEY as string, "utf-8");
+            return jwt.verify(token, publicKey)
+        } catch (error) {
+            console.error(`Invalide access token: ${error}`)
+        }
+    },
+
+    decodeAccessToken: (token: string) => {
+        try {
+            return jwt.decode(token)
+        } catch (error) {
+            console.error(`Une erreur est survenu lors du decodage: ${error}`);
+        }
+    },
+
+    // REFRESH TOKEN ET SES FONCTIONS
+    refreshToken: (payload: string) => {
+        const signOption = {
+            algorithm: process.env.JWT_ALGORITHM as jwt.Algorithm,
+            expiresIn: process.env.JWT_REFRESH_EXPIRES_IN as string
+        };
+        const privateKey = readFileSync(process.env.JWT_REFRESH_PRIVATE_KEY as string, "utf-8");
+        return jwt.sign(payload, privateKey, signOption);
+    },
+
+    verifyRefreshToken: (token: string) => {
+        try {
+            const publicKey = readFileSync(process.env.JWT_REFRESH_PRIVATE_KEY as string, "utf-8");
+            return jwt.verify(token, publicKey)
+        } catch (error) {
+            console.error(`token invalide ${error}`);
+        }
+    },
+
+    decodeRefreshToken: (token: string) => {
+        try {
+            return jwt.decode(token)
+        } catch (error) {
+            console.error(`Une erreur est survenu lors du decodage du token: ${error}`);
+        }
+    }
+};
+
+export default userToken;
